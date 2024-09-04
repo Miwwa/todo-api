843965a5f84f40383aa61a166888c9ad787be299
Mikhail <eremin.ms92@gmail.com>
2024-09-04 22:34:38 +0300

    Add graceful shutdown to HTTP server
    Refactor main function to include server timeouts and handle system signals for a graceful shutdown. Improved logging for server start, stop, and shutdown processes. This ensures the server properly handles termination signals and completes active requests before shutting down.

--- main.go
+++ main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	shutdownTimeout = 5 * time.Second
+	readTimeout     = 5 * time.Second
+	writeTimeout    = 10 * time.Second
+	idleTimeout     = 120 * time.Second
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("welcome"))
 		if err != nil {
 			return
 		}
 	})
-	err := http.ListenAndServe(":3000", r)
-	if err != nil {
-		_, err := fmt.Fprint(os.Stderr, err)
-		if err != nil {
-			return
-		}
-		return
-	}
-}
 
-// todo:
+	server := &http.Server{
+		Addr:         ":3000",
+		Handler:      router,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeout,
+	}
+
+	sigChannel := make(chan os.Signal, 1)
+	signal.Notify(sigChannel, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				log.Fatalf("Http server error: %v", err)
+			}
+		}
+		log.Println("Stopped serving new connections.")
+	}()
+	log.Print("Server Started")
+
+	<-sigChannel
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	log.Println("Graceful shutdown complete.")
+}
